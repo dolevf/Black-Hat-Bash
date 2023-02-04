f5678c076ff4dc11533c5048abe0a46b52f85b77
dolevf <farhi.dolev@gmail.com>
2023-02-04 15:06:49 -0500

    add vulns

---
+++ lab/machines/p-web-02-db/files/donate.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<h1>Welcome to ACME Hyper Alliance Donation Page.<h1>
+<h2>We appreciate any donations made towards our cause of saving cats. <h2>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="amount" autofocus id="amount" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+
+<pre>
+<?php
+    if(isset($_GET['amount']))
+    {
+        if (str_contains($_GET['amount'], ";")) { 
+            echo "Character ; is not allowed"; 
+            exit();
+        }
+        system("echo " . $_GET['amount'] . " >> amount_to_donate.txt");
+        echo "OK! your amount " . $_GET['amount'] . " will be donated to our cause. Thanks!";
+    } 
+?>
+</pre>
+</body>
+</html>
