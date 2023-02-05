6e4149d3aed47a7fb1840bb638a6212bbbe556c2
dolevf <farhi.dolev@gmail.com>
2023-02-04 21:27:44 -0500

    better installation experience

--- lab/run.sh
+++ lab/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 set -o pipefail
+
 declare -r LOG=log.txt
 
 truncate -s 0 $LOG
@@ -14,32 +16,42 @@ if [[ "$(id -u)" -ne 0 ]]; then
     fi
 fi
 
-function deploy(){
+function status(){
     local total_expected_containers
     local actual_running_containers
 
-    echo "Installing... this process can take a few minutes to complete."
+    total_expected_containers="$(grep -c container_name docker-compose.yml)"
+    actual_running_containers="$(docker-compose ps | grep -c Up)"
+
+    if [[ "$actual_running_containers" -ne "$total_expected_containers" ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+function deploy(){
+    echo "Installion started. This process can take a few minutes to complete."
+    echo "You may run \"tail -f $LOG\" to see the progress of the installation."
     # shellcheck disable=SC2129
     echo "Start Time: $(date "+%T")" >> $LOG
     # shellcheck disable=SC2024  
     sudo docker-compose up --build --detach --remove-orphans &>> $LOG 
+    
+    if status; then
+        echo "OK: all containers appear to be running. Moving on to performing unit testing..."  | tee -a $LOG
+        sleep 5
+        if python3 -m pytest -q -W ignore::DeprecationWarning tests/* &>> $LOG; then
+            echo "OK: lab appears to be up." | tee -a $LOG
+        else
+            echo "Error: some unit tests have failed."
+        fi
+    else
+        echo "Error: not all containers are running. check the log file: $LOG"
+    fi
+
     # shellcheck disable=SC2129
     echo "End Time: $(date "+%T")" >> $LOG 
-    
-    total_expected_containers="$(grep -c container_name docker-compose.yml)"
-    actual_running_containers="$(docker-compose ps | grep -c Up)"
-
-    if [[ "$actual_running_containers" -ne $total_expected_containers ]]; then
-        echo "[$actual_running_containers/$total_expected_containers] - not all containers are running. check the log file: $LOG"
-        exit 1
-    else
-        echo "All containers appear to be running. Moving on to performing unit testing..."
-    fi
-    sleep 5
-    if python3 -m pytest -q -W ignore::DeprecationWarning tests/* &>> $LOG; then
-        echo "OK: lab appears to be up."
-    fi
-    
 }
 
 function teardown(){
@@ -48,8 +60,14 @@ function teardown(){
 }
 
 function destroy(){
+    echo "Destroying the lab environment, this may take a few moments..."
     sudo docker-compose down --volumes --rmi all &> /dev/null
-    echo "OK: lab has been destroyed."
+    echo "OK: lab environment has been destroyed."
+}
+
+function rebuild(){
+    destroy
+    deploy
 }
 
 case "$1" in
@@ -62,12 +80,25 @@ case "$1" in
     destroy)
         destroy
     ;;
+    rebuild)
+        rebuild
+    ;;
+    status)
+        if status; then
+            echo "Lab is up."
+        else
+            echo "Lab is down"
+        fi
+        exit 0
+    ;;
     *)
-        echo "Usage: ./$(basename "$0") deploy|teardown|destroy"
+        echo "Usage: ./$(basename "$0") deploy|teardown|status|rebuild|destroy"
         echo
         echo "deploy   | build images and start containers"
         echo "teardown | stop containers"
         echo "destroy  | stop containers and delete containers and images"
+        echo "rebuild  | rebuilds the lab from scratch"
+        echo "status   | check the status of the lab"
         exit 1
     ;;
 esac
