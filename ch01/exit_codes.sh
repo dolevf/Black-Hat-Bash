0873e175914f029cd366028d35760374bd70e11e
dolevf <farhi.dolev@gmail.com>
2023-02-18 23:31:46 -0500

    exit codes

---
+++ ch01/exit_codes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Experimenting with status codes
+
+command="$(ls -l > /dev/null)"
+echo "The status code of the ls command was: $?"
+
+command="$(lzl 2> /dev/null)"
+echo "The status code of the non-existing lzl command was: $?"
+
+exit
