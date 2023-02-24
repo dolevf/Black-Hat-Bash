c0ae88a6d298f6dad317eacb2f7631675c4cfc8a
dolevf <farhi.dolev@gmail.com>
2023-02-24 09:44:15 -0500

    while loop example

---
+++ ch01/while_loop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SIGNAL_TO_STOP_FILE="stoploop"
+
+# This loop will run until the file "stoploop" is available.
+
+while [[ ! -f "${SIGNAL_TO_STOP_FILE}" ]]; do
+  echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exist..."
+  echo "Checking again in 2 seconds..."
+  sleep 2
+done
+
+echo "File was found! exiting..."
