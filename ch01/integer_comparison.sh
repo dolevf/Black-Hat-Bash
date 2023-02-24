a2962efc66306bee833d7ade553a5da5ebf52db1
dolevf <farhi.dolev@gmail.com>
2023-02-24 09:39:41 -0500

    integer comparison script

---
+++ ch01/integer_comparison.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VARIABLE_ONE="10"
+VARIABLE_TWO="20"
+
+if [[ "${VARIABLE_ONE}" -gt "${VARIABLE_TWO}" ]]; then
+  echo "${VARIABLE_ONE} is greater than ${VARIABLE_TWO}"
+else
+  echo "${VARIABLE_ONE} is less than ${VARIABLE_TWO}"
+fi
