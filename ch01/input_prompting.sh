f0ee2ee82d407fb267b5a8f99db1599dd51bb9ed
dolevf <farhi.dolev@gmail.com>
2023-02-24 09:36:13 -0500

    add input prompt

---
+++ ch01/input_prompting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Takes input from the user and assigns to variables.
+echo "What is your first name?"
+read -r firstname
+
+echo "What is your last name?"
+read -r lastname
+
+echo "Your first name is ${firstname} and your last name is ${lastname}"
+Listing 1-32	Accepting user input 
