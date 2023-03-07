96b335f7b9400440b96c9926396343c83a6e0d8f
dolevf <farhi.dolev@gmail.com>
2023-03-07 17:40:36 -0500

    netcat-banner-grab

---
+++ ch04/netcat-banner-grab.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+FILE="${1}"
+PORT="${2}"
+
+if [[ "$#" -ne 2 ]]; then
+  echo "Usage: ${0} <file> <port>"
+  exit 1
+fi
+
+if [[ ! -f "${FILE}" ]]; then
+  echo "File: ${FILE} was not found."
+  exit 1
+fi
+
+if [[ ! "${PORT}" -eq "${PORT}" ]]; then
+  echo "${PORT} must be a number."
+  exit 1
+fi
+
+while read -r ip; do
+  echo "Running netcat on ${ip}:${PORT}"
+  result=$(echo -e "\n" | nc -v "${ip}" -w 1 "${PORT}" 2> /dev/null)
+  if [[ -n "${result}" ]]; then
+    echo "==================="
+    echo "+ IP Address: ${ip}"
+    echo "+ Banner: ${result}"
+    echo "==================="
+  fi
+done < "${FILE}"
