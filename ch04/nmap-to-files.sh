ca8a20bb38edaa14aec63a435b5ba4031a919b4d
dolevf <farhi.dolev@gmail.com>
2023-03-06 23:33:30 -0500

    nmap to files

---
+++ ch04/nmap-to-files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+HOSTS_FILE="172-16-10-hosts.txt"
+NMAP_RESULT=$(nmap -iL ${HOSTS_FILE} --open  | grep "Nmap scan report\|tcp open")
+
+# read the nmap output line by line
+while read -r line; do
+  if echo "${line}" | grep -q "report for"; then
+    ip=$(echo "${line}" | awk -F'for ' '{print $2}')
+  else
+    port=$(echo "${line}" | grep open | awk -F'/' '{print $1}')
+    file="port-${port}.txt"
+    echo "${ip}" >> "${file}"
+  fi  
+done <<< "${NMAP_RESULT}"
