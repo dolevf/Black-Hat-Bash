ffff5317493fb0dfbf76d32794131334e5c2a415
nicholasaleks <nicholas.aleks@gmail.com>
2023-03-07 19:30:12 -0500

    Starting automated lab init script

---
+++ lab/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Black Hat Bash - Automated Lab Build Script
+ 
+# This script is intended to be executed within a Kali machine.
+# What will this script do:
+# - Installs & Configures Docker & Docker Compose
+# - Clones the Black Hat Bash Repository
+# - Builds, Tests and Runs all Lab Containers
+# - Installs & Tests all Hacking Tools used in the Black Hat Bash book
+
+# Check if sudo
+
+# Check system requirements
+# - Running Kali
+# - 8 GB of RAM
+# - 100 GB of Storage available
+# - Internet connectivity
