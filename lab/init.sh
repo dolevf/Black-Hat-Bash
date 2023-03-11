6d5f4b787aff53d285c0e96ba0d203953ccb4d9e
dolevf <farhi.dolev@gmail.com>
2023-03-10 21:44:55 -0500

    fix bugs

--- lab/init.sh
+++ lab/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Black Hat Bash - Automated Lab Build Script
- 
+
 # This script is intended to be executed within a Kali machine.
 # What will this script do:
 # - Installs & Configures Docker & Docker Compose
@@ -16,108 +16,136 @@
   # - 30 GB of Storage available
   # - Internet connectivity
 
-check_prerequisite(){
+USER_HOME_BASE="/home/${SUDO_USER}"
+BHB_BASE_FOLDER="${USER_HOME_BASE}/Black-Hat-Bash"
+BHB_LAB_FOLDER="${BHB_BASE_FOLDER}/lab"
+BHB_TOOLS_FOLDER="${USER_HOME_BASE}/tools"
+
+check_prerequisites(){
   # Checks if script is running as root
   if [[ "$EUID" -ne 0 ]]; then
-  	echo "Error: Please run as root."
+    echo "Error: Please run as root."
     exit
   fi
 
   # Check if Kali OS 
   if uname -a | grep -v -q kali; then
-  	echo "Error: Operating system does not appear to be Kali."
+    echo "Error: Operating system does not appear to be Kali."
   fi
 
   # Check internet connectivity (against Google)
-  wget -q --spider http://google.com
-  if [[ $? -ne 0 ]]; then
+  if ! wget -q --spider http://google.com; then
     echo "Error: No internet connectivity."
   fi
 
   # Check if RAM +4 GB
+  local total_ram
   total_ram=$(awk '/^MemTotal:/{print $2}' /proc/meminfo);
-  if [ $total_ram -lt 4194304 ]; then
-  	echo "Warning: System does not meet 4 GB RAM requirement."
-  	echo "This may impact the performance of the lab"
-  	read -p "Do you want to continue? [Y/N] " -n 1 -r
+  if [ "${total_ram}" -lt 4194304 ]; then
+    echo "Warning: System does not meet 4 GB RAM requirement."
+    echo "This may impact the performance of the lab"
+    read -p "Do you want to continue? [y/n] " -n 1 -r
     echo
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    if [[ ! "${REPLY}" =~ ^[Yy]$ ]]; then
       echo "Exiting..."
       exit
     fi
   fi
 
   # Check disk space
-  FREE=`df -k --output=size "$PWD" | tail -n1`
-  if [[ $FREE -lt 31457280 ]]; then
+  local free
+  free=$(df -k --output=size "$PWD" | tail -n1)
+  if [[ "${free}" -lt 31457280 ]]; then
     echo "Warning: System does not meet 30 GB disk space requirement."
-  	echo "This may impact the performance of the lab"
-  	read -p "Do you want to continue? [Y/N] " -n 1 -r
+    echo "This may impact the performance of the lab"
+    read -p "Do you want to continue? [y/n] " -n 1 -r
     echo
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    if [[ ! "${REPLY}" =~ ^[Yy]$ ]]; then
       echo "Exiting..."
       exit
     fi
-  fi;
+  fi
+
+  if [ ! -d "${BHB_TOOLS_FOLDER}" ]; then
+    mkdir "${BHB_TOOLS_FOLDER}"
+  fi
 }
 
 install_docker(){
   # Check if Docker and Docker Compose are installed.
-  docker compose version
-  if [[ ! -x "$(command -v docker)" || $? -ne 0 ]]; then
-  	echo "Installing docker..."
-  	printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
-  	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
-  	sudo apt update -y
-  	sudo apt install docker-ce docker-ce-cli containerd.io -y
-  	sudo systemctl enable docker –now
-  	sudo usermod -aG docker $USER
+  if ! docker compose version &> /dev/null; then 
+    echo "Installing docker..."
+    if [[ ! -f "/etc/apt/sources.list.d/docker-ce.list" ]]; then
+      printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
+    fi
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
+    sudo apt update -y
+    sudo apt install docker-ce docker-ce-cli containerd.io -y
+    sudo systemctl enable docker --now
+    sudo usermod -aG docker "${USER}"
   fi
 }
 
 clone_repo(){
-	# Clones the Black Hat Bash git repository locally in the home directory
-	if [[ ! -d "/home/${SUDO_USER}/Black-Hat-Bash" ]]; then
-		git clone git@github.com:dolevf/Black-Hat-Bash.git /home/${SUDO_USER}/Black-Hat-Bash
-	fi
+  # Clones the Black Hat Bash git repository locally in the home directory
+  if [[ ! -d "${BHB_BASE_FOLDER}" ]]; then
+    git clone git@github.com:dolevf/Black-Hat-Bash.git "${BHB_BASE_FOLDER}"
+  fi
 }
 
 deploy_containers(){
-	cd /home/${SUDO_USER}/Black-Hat-Bash/lab
-	./run.sh cleanup
-	./run.sh deploy
-	./run.sh status
+  if [[ -d "${BHB_LAB_FOLDER}" ]]; then
+    cd "${BHB_LAB_FOLDER}" 
+  else
+    echo "Could not find the lab folder at ${BHB_LAB_FOLDER}"
+    exit 1
+  fi
+
+  ./run.sh cleanup
+  ./run.sh deploy
+  ./run.sh status
+}
+
+install_tools(){
+  install_wappalyzer
+  install_rustscan
+  install_nuclei
+  install_linux_exploit_suggester_2
+  install_gitjacker
+  install_linenum
+  install_mimipenguin
+  install_linuxprivchecker
 }
 
 install_wappalyzer(){
   curl -sL https://deb.nodesource.com/setup_14.x | bash
-  sudo apt update
+  sudo apt update -y
   sudo apt install nodejs npm -y
-  sudo npm install –global yarn
-  cd /home/${SUDO_USER}/tools
+  sudo npm install --global yarn
+  cd "${BHB_TOOLS_FOLDER}"
   git clone https://github.com/wappalyzer/wappalyzer.git
   cd wappalyzer
   yarn install
   yarn run link
-  if [[ ! grep -q wappalyzer /home/${SUDO_USER}/.bashrc ]]; then
-    echo "alias wappalyzer='node /home/${SUDO_USER}/tools/wappalyzer/src/drivers/npm/cli.js'" >> /home/${SUDO_USER}/.bashrc
+  if ! grep -q wappalyzer "${USER_HOME_BASE}/.bashrc"; then
+    echo "alias wappalyzer='node ${BHB_TOOLS_FOLDER}/wappalyzer/src/drivers/npm/cli.js'" >> "${USER_HOME_BASE}.bashrc"
   fi
 }
 
 install_rustscan(){
   sudo apt install cargo -y
-  cd /home/${SUDO_USER}/tools
+  cd "${BHB_TOOLS_FOLDER}"
   git clone https://github.com/RustScan/RustScan.git
   cd RustScan
-  cargo build –-release
-  if [[ ! grep -q rustscan /home/${SUDO_USER}/.bashrc |  ]]; then
-    echo "alias rustscan='/home/${SUDO_USER}/tools/RustScan/target/release/rustscan'" >> /home/${SUDO_USER}/.bashrc
+  cargo build --release
+  if ! grep -q rustscan "${USER_HOME_BASE}/.bashrc" ; then
+    echo "alias rustscan='${BHB_TOOLS_FOLDER}/RustScan/target/release/rustscan'" >> "${USER_HOME_BASE}.bashrc"
   fi
 }
 
 install_nuclei(){
   sudo apt install nuclei -y
-  cd /home/${SUDO_USER}/tools
+  cd "${BHB_TOOLS_FOLDER}"
 }
 
 install_gobuster(){
@@ -125,55 +153,44 @@ install_gobuster(){
 }
 
 install_linux_exploit_suggester_2(){
-  cd /home/${SUDO_USER}/tools
+  cd "${BHB_TOOLS_FOLDER}"
   git clone https://github.com/jondonas/linux-exploit-suggester-2.git
 }
 
 install_gitjacker(){
   sudo apt install jq -y
   curl -s "https://raw.githubusercontent.com/liamg/gitjacker/master/scripts/install.sh" | bash
-  if [[ ! grep -q gitjacker /home/${SUDO_USER}/.bashrc ]]; then
-    echo "alias gitjacker='/home/${SUDO_USER}/bin/gitjacker'" >> /home/${SUDO_USER}/.bashrc
+  if ! grep -q gitjacker "${USER_HOME_BASE}.bashrc"; then
+    echo "alias gitjacker='${USER_HOME_BASE}/bin/gitjacker'" >> "${USER_HOME_BASE}.bashrc"
   fi
 }
 
 install_linenum(){
-  cd /home/${SUDO_USER}/tools
+  cd "${BHB_TOOLS_FOLDER}"
   wget https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh
   chmod u+x LinEnum.sh
 }
 
 install_mimipenguin(){
-  cd /home/${SUDO_USER}/tools
+  cd "${BHB_TOOLS_FOLDER}"
   git clone https://github.com/huntergregal/mimipenguin.git
 }
 
 install_linuxprivchecker(){
-  cd /home/${SUDO_USER}/tools
+  cd "${BHB_TOOLS_FOLDER}"
   git clone https://github.com/sleventyeleven/linuxprivchecker.git
 }
 
+export DEBIAN_FRONTEND=noninteractive # don't prompt user with TUI consoles
+
 echo "Initializing Black Hat Bash Automated Lab Build Script..."
 echo
-check_prerequisite
 
+# Run steps
+check_prerequisites
 install_docker
-
 clone_repo
-
 deploy_containers
-
-if [ ! -d /home/${SUDO_USER}/tools ]; then
-	mkdir /home/${SUDO_USER}/tools
-fi
-
-install_wappalyzer
-install_rustscan
-install_nuclei
-install_linux_exploit_suggester_2
-install_gitjacker
-install_linenum
-install_mimipenguin
-install_linuxprivchecker
+install_tools
 
 source ~/.bashrc
