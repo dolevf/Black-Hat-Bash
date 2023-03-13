432bf9c928397280fd9750ba776f6e640278f42f
dolevf <farhi.dolev@gmail.com>
2023-03-12 22:13:16 -0400

    notifier

---
+++ ch04/host_monitor_notification.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# sends a notification upon new host discovery
+KNOWN_HOSTS="172-16-10-hosts.txt"
+NETWORK="172.16.10.0/24"
+INTERFACE="br_public"
+FROM_ADDR="kali@blackhatbash.com"
+TO_ADDR="security@blackhatbash.com"
+
+while true; do 
+  echo "Performing an ARP scan against ${NETWORK}..."
+  sudo arp-scan -x ${NETWORK} -I ${INTERFACE} | while read -r line; do
+    host="$(echo "${line}" | awk '{print $1}')"
+    if ! grep -q "${host}" "${KNOWN_HOSTS}"; then
+      echo "Found a new host: ${host}!"
+      echo "${host}" >> "${KNOWN_HOSTS}"
+      sendemail -f "${FROM_ADDR}" \
+        -t "${TO_ADDR}" \
+		-u "ARP Scan Notification" \
+		-m "A new host found: ${host}"
+    fi
+  done 
+  sleep 10
+done
