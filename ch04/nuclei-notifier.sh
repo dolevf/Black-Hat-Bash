448a5bd18651f26485e4457cf3e2e436657bf716
dolevf <farhi.dolev@gmail.com>
2023-03-19 00:38:06 -0400

    nuclei notifier script

---
+++ ch04/nuclei-notifier.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+EMAIL_TO="security@blackhatbash.com"
+EMAIL_FROM="nuclei-automation@blackhatbash.com"
+
+for ip_address in "$@"; do
+  echo "Testing ${ip_address} with Nuclei..."
+  result=$(nuclei -u "${ip_address}" -silent -severity medium,high,critical)
+  if [[ -n "${result}" ]]; then 
+    while read -r line; do
+      template=$(echo "${line}" | awk '{print $1}' | tr -d '[]')
+      url=$(echo "${line}" | awk '{print $4}')
+      echo "Sending an email with the findings ${template} ${url}"
+      sendemail -f "${EMAIL_FROM}" \
+      		-t "${EMAIL_TO}" \
+                -u "[Nuclei] Vulnerability Found!" \
+                -m "${template} - ${url}"
+                
+    done <<< "${result}"
+  fi
+done
