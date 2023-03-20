359fb9cd58d4008f9886ecb3b3a852702d9c4bf8
dolevf <farhi.dolev@gmail.com>
2023-03-20 14:12:40 -0400

    correct the personas

--- lab/provision.sh
+++ lab/provision.sh
@@ -5,7 +5,7 @@ p_web_02() {
 
   # Provision WordPress (p-web-02)
   result=$(curl -s -X POST \
-          -d 'weblog_title=ACME Impact Alliance&user_name=mwilliams&first_name=Michael&last_name=Williams&admin_password=G#7$tFArM!nD&admin_password2=G#7$tFArM!nD&admin_email=mwilliams@acme-infinity-servers.com&blog_public=0&Submit=Install WordPress&language=""' \
+          -d 'weblog_title=ACME Impact Alliance&user_name=jtorres&first_name=Jane&last_name=Torres&admin_password=asfim2ne7asd7&admin_password2=asfim2ne7asd7&admin_email=jtorres@acme-impact-alliance.com&blog_public=0&Submit=Install WordPress&language=""' \
           "http://172.16.10.12/wp-admin/install.php?step=2")
   if ! echo "${result}" | grep -q "WordPress has been installed. Thank you"; then
     echo "Error provisioning WordPress (p-web-02)"
