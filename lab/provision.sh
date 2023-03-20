abfdfe043b1e5349e762777c0c56800b8d192ee7
dolevf <farhi.dolev@gmail.com>
2023-03-20 13:24:39 -0400

    rename admin user

--- lab/provision.sh
+++ lab/provision.sh
@@ -5,7 +5,7 @@ p_web_02() {
 
   # Provision WordPress (p-web-02)
   result=$(curl -s -X POST \
-          -d 'weblog_title=ACME Impact Alliance&user_name=admin&admin_password=wAWSD@ASw2&admin_password2=wAWSD@ASw2&admin_email=dbrown@acme-impact-alliance.com&blog_public=0&Submit=Install WordPress&language=""' \
+          -d 'weblog_title=ACME Impact Alliance&user_name=mwilliams&first_name=Michael&last_name=Williams&admin_password=G#7$tFArM!nD&admin_password2=G#7$tFArM!nD&admin_email=mwilliams@acme-infinity-servers.com&blog_public=0&Submit=Install WordPress&language=""' \
           "http://172.16.10.12/wp-admin/install.php?step=2")
   if ! echo "${result}" | grep -q "WordPress has been installed. Thank you"; then
     echo "Error provisioning WordPress (p-web-02)"
