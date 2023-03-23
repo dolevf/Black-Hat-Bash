505d9f340d83b9df4dbd078c8193dc4c3198de71
dolevf <farhi.dolev@gmail.com>
2023-03-22 22:18:02 -0400

    port watchdog update

--- ch04/port_watchdog.sh
+++ ch04/port_watchdog.sh
@@ -14,15 +14,16 @@ service_discovery(){
 }
 
 while true; do 
-    port_scan=$("${RUST_SCAN_BIN}" -a "${IP_ADDRESS}" -g -p "${WATCHED_PORT}")
-    if [[ -n "${port_scan}" ]]; then
-      echo "${IP_ADDRESS} has started responding on port ${WATCHED_PORT}!"
-      echo "Performing a service discovery..."
-      if service_discovery "${IP_ADDRESS}" "${WATCHED_PORT}"; then
-        echo "Wrote port scan data to ${LOG_FILE}"
-      fi
-    else
-      echo "Port not yet open or was closed, sleeping for 5 seconds..."
-      sleep 5
+  port_scan=$("${RUST_SCAN_BIN}" -a "${IP_ADDRESS}" -g -p "${WATCHED_PORT}")
+  if [[ -n "${port_scan}" ]]; then
+    echo "${IP_ADDRESS} has started responding on port ${WATCHED_PORT}!"
+    echo "Performing a service discovery..."
+    if service_discovery "${IP_ADDRESS}" "${WATCHED_PORT}"; then
+      echo "Wrote port scan data to ${LOG_FILE}"
+      break
     fi
+  else
+    echo "Port not yet open or was closed, sleeping for 5 seconds..."
+    sleep 5
+  fi
 done
