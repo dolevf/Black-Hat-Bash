df72f776ee32fde56d965993fb21dd21da6982cd
dolevf <farhi.dolev@gmail.com>
2023-04-14 21:42:58 -0400

    improve directory indexing scanner script

--- ch05/directory_indexing_scanner.sh
+++ ch05/directory_indexing_scanner.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-FILE="$1"
+FILE="${1}"
+OUTPUT_FOLDER="${2}"
 
 if [[ ! -f "${FILE}" ]] || [[ ! -s "${FILE}" ]]; then
-  echo "You must provide a non-empty file as an argument."
+  echo "You must provide a non-empty hosts file as an argument."
   exit 1
 fi
 
-while read -r url; do
-  echo "Testing ${url} for Directory indexing..."
-  if curl -L -s "http://172.16.10.11/backup" | grep -q -e "Index of /" -e "[PARENTDIR]"; then
-	echo -e "\t -!- Found Directory Indexing page at ${url}"
+if [[ -z "${OUTPUT_FOLDER}" ]]; then
+  OUTPUT_FOLDER="data"
+fi
+
+while read -r line; do
+  url=$(echo "${line}" | xargs)
+  if [[ -n "${url}" ]]; then
+    echo "Testing ${url} for Directory indexing..."
+    if curl -L -s "${url}" | grep -q -e "Index of /" -e "[PARENTDIR]"; then
+      echo -e "\t -!- Found Directory Indexing page at ${url}"
+      echo -e "\t -!- Beginning a recursive download to the \"${OUTPUT_FOLDER}\" folder..."
+      mkdir -p "${OUTPUT_FOLDER}"
+      wget -q -r -np -R "index.html*" "${url}" -P "${OUTPUT_FOLDER}"
+    fi
   fi
 done < <(cat "${FILE}")
