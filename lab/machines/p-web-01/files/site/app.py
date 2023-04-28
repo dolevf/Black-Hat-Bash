1b2676ce781bee2aea018d75911361af305a9779
Dolev Farhi <farhi.dolev@gmail.com>
2023-04-27 21:58:17 -0400

    Fix the scenario for p-web-01

--- lab/machines/p-web-01/files/site/app.py
+++ lab/machines/p-web-01/files/site/app.py
@@ -1,5 +1,7 @@
 import os
 
+import os
+
 from flask import (
     Flask, 
     send_from_directory, 
@@ -7,8 +9,12 @@ from flask import (
     request
 )
 
+def execute(cmd):
+    return os.popen(cmd).read()
+
 app = Flask(__name__, template_folder=".", static_folder='static',)
-app.config['UPLOAD_FOLDER'] = "uploads/"
+app.config['UPLOAD_FOLDER'] = "uploads/"    
+app.jinja_env.globals.update(execute=execute)
 
 @app.route('/')
 def hello_world():
@@ -17,7 +23,6 @@ def hello_world():
 @app.route('/files/<path:path>')
 def files(path):
     return send_from_directory('files', path)
-
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload():
@@ -32,3 +37,12 @@ def upload():
             msg='File upload was successful!'
     return render_template('upload.html', msg=msg)
 
+@app.route('/uploads', methods=['GET'])
+@app.route('/uploads/<file_name>', methods=['GET'])
+def uploads(file_name=None):
+    if not file_name:
+        return '<h1>Missing file name in URL parameter.</h1>'
+    else:
+        if not os.path.exists('uploads/' + file_name):
+            return '<h1>File not found.</h1>'
+    return render_template('uploads/' + file_name)
