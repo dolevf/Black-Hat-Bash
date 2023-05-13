3f42fe8119f8dfd324b2c153f56ba08ab3d7edad
nicholasaleks <nicholas.aleks@gmail.com>
2023-05-13 13:53:50 -0400

    Designed ACME Impact Alliance WP site

--- lab/machines/p-web-02/files/site/index.php
+++ lab/machines/p-web-02/files/site/index.php
@@ -1,17 +1,70 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ACME Impact Alliance Charity Website</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <!-- Custom styles -->
+  <style>
+    .hero-image {
+      background-image: url('charity.jpg');
+      background-size: cover;
+      background-position: center;
+      height: 500px;
+    }
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+    .hero-content {
+      text-align: center;
+      padding: 150px 0;
+      color: #fff;
+    }
+  </style>
+</head>
+<body>
+  <header>
+    <!-- Navigation bar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">ACME Impact Alliance</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/donate.php">Donate</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+  <section class="hero-image">
+    <div class="hero-content">
+      <h1>Welcome to the ACME Impact Alliance charity</h1>
+      <p>Help us make a difference in the lives of all cats.</p>
+      <a href="/donate.php" class="btn btn-primary">Donate Now</a>
+    </div>
+  </section>
+
+  <section class="container">
+    <h2>About Us</h2>
+    <p>Welcome to ACME Impact Alliance, a non-profit organization dedicated to making a positive impact in the lives cats in need. At our charity, we believe that every cat deserves a happy home. We work tirelessly towards our cause.</p>
+
+    <p>Since our establishment in 2023, we have been actively involved in numerous projects and initiatives. Through the generosity and support of individuals like you, we have been able to have a positive impact through the charity's work.</p>
+
+    <p>Our dedicated team of volunteers and staff are committed to our cat cause. We strive to create sustainable solutions that empower cat communities and transform all feline lives. By collaborating with local partners and leveraging resources effectively, we maximize the impact of each donation and effort.</p>
+
+    <p>We believe in transparency and accountability, ensuring that every contribution we receive is utilized efficiently and ethically. ACME Impact Alliance is proud to be recognized for our cat commitments.</p>
+
+    <p>Join us in our mission. Whether through donations, volunteering, or spreading awareness, your support can make a significant difference in the lives of the cats we serve. Together, we can create a brighter future for all cats.</p>
+
+    <p>Thank you for your continued support and belief in our vision. Together, we can make a difference and build a better world for cats everywhere.</p>
+  </section>
+
+  <!-- Bootstrap JS (Optional) -->
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
