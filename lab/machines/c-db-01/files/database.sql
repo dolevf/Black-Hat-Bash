02bda1f66cd3c45242924d45229b9499bc205fca
Dolev Farhi <farhi.dolev@gmail.com>
2023-05-28 15:08:33 -0400

    Fix db01 not loading on system reboot

--- lab/machines/c-db-01/files/database.sql
+++ lab/machines/c-db-01/files/database.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE adminer_db;
-CREATE USER 'adminer_user'@'localhost' IDENTIFIED BY 'P@ssword321';
+CREATE DATABASE IF NOT EXISTS adminer_db;
+CREATE USER IF NOT EXISTS 'adminer_user'@'localhost' IDENTIFIED BY 'P@ssword321';
 GRANT ALL ON adminer_db.* TO 'adminer_user'@'localhost';
