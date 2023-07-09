4840ff7cdaab1388d6cb210dda1fbe792502d408
Dolev Farhi <farhi.dolev@gmail.com>
2023-07-09 18:07:30 -0400

    add os detect script and add lshw to dockerfile base

---
+++ ch08/os_detect.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+OS="$(uname)"
+
+case $OS in
+  "Linux")
+    OS="Linux"
+    ;;
+  "FreeBSD")
+    OS="FreeBSD"
+    ;;
+  "WindowsNT")
+    OS="Windows"
+    ;;
+  "Darwin") 
+    OS="Mac"
+    ;;
+  *) 
+    OS="Unknown"
+  ;;
+esac
+
+echo "The detected operating system is ${OS}"
