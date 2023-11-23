d25178df04da86c7fc2581d18d15d01ea5ab32de
Dolev Farhi <farhi.dolev@gmail.com>
2023-11-22 23:09:46 -0500

    Update test_if_file_exists.sh

--- ch02/test_if_file_exists.sh
+++ ch02/test_if_file_exists.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-# Sets a variable containing the file name.
 FILENAME="flow_control_with_if.txt"
 
-# Test if a file exists, if not, create it.
 if [[ -f "${FILENAME}" ]]; then
   echo "${FILENAME} already exists."
   exit 1
