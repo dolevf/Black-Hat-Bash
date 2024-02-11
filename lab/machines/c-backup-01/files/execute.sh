8ccb68492ec0c83511f6a7ef75a4340554af512b
dolevf <farhi.dolev@gmail.com>
2024-02-10 23:54:35 -0500

    remove duplicate word

--- lab/machines/c-backup-01/files/execute.sh
+++ lab/machines/c-backup-01/files/execute.sh
@@ -6,7 +6,7 @@ LOG="/tmp/job.log"
 
 echo "$(date) - Starting cleanup script..." >> "$LOG"
 if find /tmp -type f ! -name 'job.log' -exec rm -rf {} +; then
-    echo "cleaned up files from the the /tmp folder."  >> "$LOG"
+    echo "cleaned up files from the /tmp folder."  >> "$LOG"
 fi
 
 echo "$(date) - Cleanup script is finished." >> "$LOG"
