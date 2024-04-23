2eab7a4703dc5c18b330e7a5eb8bc90d803dfd28
Dolev Farhi <farhi.dolev@gmail.com>
2024-04-22 23:36:04 -0400

    spacing

--- ch09/gnupg_passphrase_bf.sh
+++ ch09/gnupg_passphrase_bf.sh
@@ -9,14 +9,14 @@ fi
 while read -r passphrase; do
   echo "Brute forcing with ${passphrase}..."
   if echo "${passphrase}" | gpg --batch \
-                             --yes \
-                             --pinentry-mode loopback \
-                             --passphrase-fd 0 \
-                             --output private.pgp \
-                             --armor \
-                             --export-secret-key "${KEY_ID}"; then
-      echo "Passphrase is: ${passphrase}"
-      echo "Private key is located at private.pgp"
-      exit 0
+                                --yes \
+                                --pinentry-mode loopback \
+                                --passphrase-fd 0 \
+                                --output private.pgp \
+                                --armor \
+                                --export-secret-key "${KEY_ID}"; then
+       echo "Passphrase is: ${passphrase}"
+       echo "Private key is located at private.pgp"
+       exit 0
   fi
 done < passphrases.txt
